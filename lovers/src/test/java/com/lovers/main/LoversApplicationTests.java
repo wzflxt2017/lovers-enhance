595f0ce671519f579da1d2d5b39bf7a632ffdecc
wangzefengWindows <wzflxt2017@163.com>
2020-01-07 18:14:48 +0800

    创建项目

---
+++ lovers/src/test/java/com/lovers/main/LoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovers.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
